958d6c7e0f88880be99e1c4aadf1e7ab6493ded7
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-10-06 01:00:54 +0900

    click

--- FRONT/js/sendRequest.js
+++ FRONT/js/sendRequest.js
@@ -7,16 +7,18 @@ $(function (){
 				lat : 35.6641222,
 				lon : 139.729426
 			},
+			beforeSend: function() {
+				$("#result").html("loading...");
+			},
 			success: function( data ) {
 				if (data.error == 1) {
-					console.log("error!");
+					$("#result").html("error");
 				} else {
-					//console.log(data["result"]);
+					$("#result").html("");
 					for (var i=0 ; i<data["result"].length ; i++ ) {
 						var title = data["result"][i]["title"];
 						var lat = data["result"][i]["lat"];
 						var lon = data["result"][i]["lon"];
-						console.log("title:" + title + "&lat:" + lat + "&lon:" + lon);
 						$("#result").append("title:" + title + "&lat:" + lat + "&lon:" + lon + "<br>\n");
 					}
 				}
