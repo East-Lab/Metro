fa3c887c01de1a625ae8c48b206e26e1b13cb145
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-10-06 00:41:42 +0900

    click

--- FRONT/js/sendRequest.js
+++ FRONT/js/sendRequest.js
@@ -1,4 +1,18 @@
 $(function (){
 	$("#hoge").append("aaaaaaaaaaa");
-	console.log("hoge");
 });
+
+$("#btn").onClick(function(){
+	console.log("click");
+	$.ajax({
+		url: "/metro/API/getMetroPOI.php",
+		data: {
+			lat : 35.6641222,
+			lon : 139.729426
+		},
+		success: function( data ) {
+			console.log(data);
+		}
+	});
+});
+
