b8ac3f7efa13d7b77c72b73f38b506b03a289086
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-10-06 23:38:38 +0900

    class開放

--- API/index.php
+++ API/index.php
@@ -1,16 +1,16 @@
 <?php
 
-require_once("/home/gif-animaker/Metro/API/class/MetroAPI.php");
+metrouire_once("/home/gif-animaker/Metro/API/class/MetroAPI.php");
 
 $metro = new MetroAPI();
 
-///$contents = $req->getTrainLocation();
-//$contents = $req->getTrainInfo();
-//$contents = $req->getStationTimetable('TokyoMetro.Chiyoda.Otemachi');
-//$contents = $req->getStationFacility();
-$contents = $req->getStation();
-//$contents = $req->getPoiByLocation(35.681265,139.766926,1000);
+///$contents = $metro->getTrainLocation();
+//$contents = $metro->getTrainInfo();
+//$contents = $metro->getStationTimetable('TokyoMetro.Chiyoda.Otemachi');
+//$contents = $metro->getStationFacility();
+$contents = $metro->getStation();
+//$contents = $metro->getPoiByLocation(35.681265,139.766926,1000);
 //$contents = $metro->getPoi();
-//$contents = $req->getStationByLocation(35.681265,139.766926,1000);
+//$contents = $metro->getStationByLocation(35.681265,139.766926,1000);
 //var_dump($contents);
 echo json_encode($contents);
