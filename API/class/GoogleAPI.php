0c05aff1c18d937ef4d3655eff0a519acdd4621f
Daichi Kasahima <daichi0.0daichi@gmail.com>
2014-10-19 16:53:37 +0900

    google API

--- API/class/GoogleAPI.php
+++ API/class/GoogleAPI.php
@@ -3,26 +3,29 @@ class GoogleAPI{
 	private $baseurl = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json';
 
 	public function getAroundPlace($lat, $lon, $radius){
-		$data['lat'] = $lat; 
-		$data['lon'] = $lon; 
+		$data['location'] = $lat . "," . $lon; 
 		$data['radius'] = $radius; 
 		$contents = json_decode($this->sendRequest($data), true);
-		return $contents;
+		$arr = array();
+		foreach ($contents["results"] as $res) {
+			$res_arr = array(
+				"name" => $res["name"],
+				"location" => $res["geometry"]["location"],
+			);
+			$arr[] = $res_arr;
+		}
+		return $arr;
 	}
 
 	private function sendRequest($data) {
 		$accessToken = file_get_contents("/home/gif-animaker/Metro/API/google.key");
 		$accessToken = str_replace(array("\r\n","\r","\n"), '', $accessToken);
-		$url = $this->baseurl;
+		$url = $this->baseurl . "?language=ja&key=$accessToken&location={$data["location"]}&radius={$data["radius"]}&sensor={$data["sensor"]}";
 
-		$data['key'] = $accessToken;
-
-		$options = array('http' => array(
+		$options = array('https' => array(
 			'method' => 'GET',
-			'content' => http_build_query($data),
 		));
 		$contents = file_get_contents($url, false, stream_context_create($options));
-
 		return $contents;
 	}
 }
