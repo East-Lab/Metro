2b9215a83c9c098f7d3f6ae4a30bfcd4c608ed53
stmng629 <stmng629@gmail.com>
2014-10-19 17:13:11 +0900

    add

--- FRONT/js/googleMapsMetroDev.js
+++ FRONT/js/googleMapsMetroDev.js
@@ -118,6 +118,57 @@
         homeControlDiv.index = 1;
         gmap.controls[google.maps.ControlPosition.TOP_RIGHT].push(homeControlDiv);
 
+        // Place検索
+        var input = (document.getElementById('pac-input'));
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', gmap);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: gmap,
+          anchorPoint: new google.maps.Point(0, -29)
+        });
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+          infowindow.close();
+          marker.setVisible(false);
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+
+          // If the place has a geometry, then present it on a map.
+          if (place.geometry.viewport) {
+            gmap.fitBounds(place.geometry.viewport);
+          } else {
+            gmap.setCenter(place.geometry.location);
+            gmap.setZoom(17);  // Why 17? Because it looks good.
+          }
+          marker.setIcon(/** @type {google.maps.Icon} */({
+            url: place.icon,
+            size: new google.maps.Size(71, 71),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(17, 34),
+            scaledSize: new google.maps.Size(35, 35)
+          }));
+          marker.setPosition(place.geometry.location);
+          marker.setVisible(true);
+
+          var address = '';
+          if (place.address_components) {
+            address = [
+              (place.address_components[0] && place.address_components[0].short_name || ''),
+              (place.address_components[1] && place.address_components[1].short_name || ''),
+              (place.address_components[2] && place.address_components[2].short_name || '')
+            ].join(' ');
+          }
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+          infowindow.open(gmap, marker);
+        });
+
+
 
         directionsDisplay.setMap(gmap);
         //alert("init map end");
