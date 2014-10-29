8fbe9cd32eb5c70fd149ba7b139c4727505cfddd
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-10-29 22:12:04 +0900

    最寄り駅の近くのplaceAPI

--- API/getAroundStationPlace.php
+++ API/getAroundStationPlace.php
@@ -34,6 +34,7 @@ $metro = new MetroAPI();
 $contents = $metro->getPoiByLocation($arg["lat"],$arg["lon"], $arg["radius"], 1);
 $poi = $contents['result'][0]['title'];
 $station = str_replace(strstr($poi, "出入口"),'',$poi);
+echo $station;
 
 
 
