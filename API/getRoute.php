a5236fb5102acca211502f96854ba06da4765d39
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-11-03 00:44:41 +0900

    route api modified

--- API/getRoute.php
+++ API/getRoute.php
@@ -38,6 +38,8 @@ $google = new GoogleAPI();
 $url = "http://maps.googleapis.com/maps/api/directions/json?origin={$arg["latA"]},{$arg["lonA"]}&destination={$arg["latB"]},{$arg["lonB"]}&mode=walking&sensor=false";
 $contents = $google->sendRequest($url);
 $data = json_decode($contents, true);
+var_dump($data["routes"][0]["legs"][0]["steps"]);
+exit(0);
 $steps = $data["routes"][0]["legs"][0]["steps"];
 $arr = array();
 foreach ($steps as $s) {
@@ -47,8 +49,8 @@ foreach ($steps as $s) {
 	);
 } 
 $arr[count($steps) - 1] = array(
-	"lat" => $data["routes"][0]["legs"][0]["end_location"]["lat"],
-	"lon" => $data["routes"][0]["legs"][0]["end_location"]["lng"],
+	"lat" => $data["routes"][0]["legs"]["end_location"]["lat"],
+	"lon" => $data["routes"][0]["legs"]["end_location"]["lng"],
 );
 if ($arg["escape"]) {
 	echo json_encode($arr, JSON_UNESCAPED_UNICODE);
