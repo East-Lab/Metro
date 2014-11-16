8adad96d9183faf731db52652ba6d17d2477e13c
kzhrknt <kuzuhara.1989@gmail.com>
2014-11-17 00:04:17 +0900

    add website and screenshot

---
+++ MetroadWeb/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <?php include('meta.php'); ?>
+  <body>
+    <?php include('header.php'); ?>
+    <?php include('introduction.php'); ?>
+    <?php include('step.php'); ?>
+    <?php include('members.php'); ?>
+    <?php include('footer.php'); ?>
+  </body>
+</html>
