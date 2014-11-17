b261b1bd2f632fe0f75a855d03571f567ddb3041
kzhrknt <kuzuhara.1989@gmail.com>
2014-11-18 06:26:45 +0900

    add img&movie files

--- submit/5.application-url/metroad/introduction.php
+++ submit/5.application-url/metroad/introduction.php
@@ -20,7 +20,7 @@
              <style>.embed-container {position: relative; padding-bottom: 100%; height: 0; overflow: hidden;} .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe width='100%' src='https://vine.co/v/OMQMIxUiF6Y/embed/simple' frameborder='0' scrolling='no' allowtransparency='true'></iframe><script async src='//platform.vine.co/static/scripts/embed.js' charset='utf-8'></script></div>
          </div>
          -->
-         <iframe width="480" height="360" src="//www.youtube.com/embed/NZCv38i9mcQ" frameborder="0" allowfullscreen></iframe>
+<style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; height: auto; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style><div class='embed-container'><iframe src='http://www.youtube.com/embed/NZCv38i9mcQ' frameborder='0' allowfullscreen></iframe></div>
 
        </div>
     </div><!-- row -->
